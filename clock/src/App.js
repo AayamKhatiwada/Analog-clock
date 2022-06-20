a87198931f6c6687a7973ae1e5673e63599e1a56
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-06-20 16:06:30 +0545

    Adding css and html code

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
+  return (    
+    <div className="clock">
+      <div className='hand hour'></div>
+      <div className='hand minute'></div>
+      <div className='hand second'></div>
+
+        <div className='number number1'>1</div>
+        <div className='number number2'>2</div>
+        <div className='number number3'>3</div>
+        <div className='number number4'>4</div>
+        <div className='number number5'>5</div>
+        <div className='number number6'>6</div>
+        <div className='number number7'>7</div>
+        <div className='number number8'>8</div>
+        <div className='number number9'>9</div>
+        <div className='number number10'>10</div>
+        <div className='number number11'>11</div>
+        <div className='number number12'>12</div>
+
     </div>
   );
 }
